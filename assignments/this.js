cfe21703fa7007bb86fc187927b0d4b63450ce74
DeAndre Johnson <dre@phreshr.com>
2019-09-19 03:50:45 +0800

    explained the 4 principles of `this` and gave examples

--- assignments/this.js
+++ assignments/this.js
@@ -10,17 +10,54 @@
 */
 
 // Principle 1
+/* 'this' is bound to the global scope which is the Window/Global object */
 
 // code example for Window Binding
+function sayHello(greeting) {
+  console.log('Global: ', this);
+  console.log(`${greeting} world!`)
+}
+
+sayHello('Hello')
 
 // Principle 2
+/* 'When a method is called on an object, 'this' is bound to that object before the dot */
 
 // code example for Implicit Binding
+const myObj = {
+  name: 'Dre',
+  sayHello: function() {
+    console.log('Implicit: ', this);
+    console.log(`Hello ${this.name}`);
+  }
+}
+
+myObj.sayHello()
 
 // Principle 3
-
+/* 'this' is bound to a specific instance of a object that was created by a constructor function */ 
 // code example for New Binding
+function Person(attrs) {
+  this.name = attrs.name
+  this.sayHello = function() {
+    console.log('New Binding: ', this);
+    console.log(`Hello ${this.name}`);
+  }
+}
+
+const me = new Person({
+  name: 'Dre',
+})
+
+me.sayHello()
 
 // Principle 4
+/* By using 'call' and 'apply', Explicit Binding let you overide what object 'this' refers to */ 
 
 // code example for Explicit Binding
+function sayHi() {
+  console.log('Explicit: ', this);
+  console.log(`Hi ${this.name}`);
+}
+
+sayHi.call(me)
